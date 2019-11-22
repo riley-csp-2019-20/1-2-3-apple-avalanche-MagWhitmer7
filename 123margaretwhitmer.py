dd54739f6b1e89797cd9e9c9b8a7726d3acda7ac
MagWhitmer7 <55501077+MagWhitmer7@users.noreply.github.com>
2019-11-22 11:35:38 -0500

    Add files via upload

--- 123margaretwhitmer.py
+++ 123margaretwhitmer.py
@@ -8,31 +8,38 @@ wn = trtl.Screen()
 wn.addshape(apple_image) # Make the screen aware of the new file
 wn.setup(width=1.0, height=1.0)
 
-apple = trtl.Turtle()
+#apple = trtl.Turtle()
 
 letter_list = ["a", "s", "d", "f", "g", "h", "j", "k", "l", "x"]
 
-letter = []
+current_letters = []
 
+apple_list = []
+
+number_of_apples = 5
 
 # given a turtle, set that turtle to be shaped by the image file
-def draw_apple(active_apple):
-  active_apple.shape(apple_image)
-  wn.update()
+#def draw_apple(active_apple):
+  #active_apple.shape(apple_image)
+  #wn.update()
 
-draw_apple(apple)
+#draw_apple(apple)
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.bgpic("tree.gif")
 
 def apple_fall():
+  index = current_letters.index(letter)
+  current_letters.pop(index)
+  apple = apple_list.pop(index)
   apple.showturtle()
   apple.penup()
   apple.goto(0,-150)
   drawer.clear()
   corner_drawer.clear()
   apple.hideturtle()
+  apple_list.append(apple)
   move_apple()
 
 
@@ -43,7 +50,18 @@ drawer.hideturtle()
 def draw_the_letter():
   drawer.color("blue")
   drawer.penup()
-  drawer.write(letter, font=("Arial", 74, "bold")) 
+  drawer.write(current_letters, font=("Arial", 74, "bold")) 
+
+def reset_apple():
+    new_xpos= rand.randint(-100,100)
+    new_ypos= rand.randint(-50,50)
+    apple.goto(new_xpos, new_ypos)
+
+for i in range(number_of_apples):
+  apple = trtl.Turtle(shape = apple_image)
+  apple.penup()
+  reset_apple()
+  apple_list.append(apple)
 
 # this tells the user what letter to type
 
@@ -51,12 +69,11 @@ corner_drawer = trtl.Turtle()
 corner_drawer.hideturtle()
 def letter_to_type():
   random = rand.randint(0, len(letter_list))
-  letter = letter_list[random]
-  letter_list.pop(random)
+  current_letters.append(letter_list[random])
   corner_drawer.color("black")
   corner_drawer.penup()
   corner_drawer.goto(0,100)
-  corner_drawer.write(letter, font=("Arial", 74, "bold"))
+  corner_drawer.write(current_letters, font=("Arial", 74, "bold"))
 
 #TODO Create a function that draws a new letter from the letter list.
 
@@ -65,21 +82,59 @@ def letter_to_type():
 # a new location on the tree, only if the list of letters is not empty. 
 
 def move_apple():
-  if 0 <= len(letter_list):
+  length_of_list = len(letter_list)
+  if (length_of_list != 0):
     letter_to_type()
     apple.showturtle()
     apple.penup()
-    new_xpos= rand.randint(-100,100)
-    new_ypos= rand.randint(-50,50)
-    apple.goto(new_xpos, new_ypos)
-    wn.onkeypress(apple_fall, letter)
-    
+    reset_apple()
+      
 
 # This call to the onkeypress function sets draw_an_A as the function
 # that will be called when the "a" key is pressed.
-move_apple()
 
 
+
+def check_apple_a():
+  if ("a" in current_letters):
+    apple_fall()
+def check_apple_s():
+  if ("s" in current_letters):
+    apple_fall()
+def check_apple_d():
+  if ("d" in current_letters):
+    apple_fall()
+def check_apple_f():
+  if ("f" in current_letters):
+    apple_fall()
+def check_apple_g():
+  if ("g" in current_letters):
+    apple_fall()
+def check_apple_h():
+  if ("h" in current_letters):
+    apple_fall()
+def check_apple_j():
+  if ("j" in current_letters):
+    apple_fall()
+def check_apple_k():
+  if ("k" in current_letters):
+    apple_fall()
+def check_apple_l():
+  if ("l" in current_letters):
+    apple_fall()
+def check_apple_x():
+  if ("x" in current_letters):
+    apple_fall()
+wn.onkeypress(check_apple_a, "a")
+wn.onkeypress(check_apple_s, "s")
+wn.onkeypress(check_apple_d, "d")
+wn.onkeypress(check_apple_f, "f")
+wn.onkeypress(check_apple_g, "g")
+wn.onkeypress(check_apple_h, "h")
+wn.onkeypress(check_apple_j, "j")
+wn.onkeypress(check_apple_k, "k")
+wn.onkeypress(check_apple_l, "l")
+wn.onkeypress(check_apple_x, "x")
 
 #TODO Create a function that takes a turtle (apple) as its parameter
 # and set that turtle to be shaped by the image file, call the letter drawing function,
